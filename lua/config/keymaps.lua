9c91d6564d019a5f81db1f92e4c286a2c659cb62
coleknutson1 <cole.knutson1@gmail.com>
2025-10-29 21:59:14 -0500

    WIP

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,3 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-- Fat-finger prevention
+vim.cmd("command W w")
+vim.cmd("command Wq wq")
